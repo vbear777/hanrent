c7c289fe162ed99bf036513b9339bc6d70114434
Vbear <ryoitenkaii@gmail.com>
2026-04-27 15:41:19 +0700

    create Hero section and initialize some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import Cars from './pages/Cars';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <div>
-      <NavBar />
-    </div>
+    <>
+      {!isOwnerPath && <NavBar setShowLogin={setShowLogin} />}
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/car-details/:id' element={<CarDetails />} />
+      <Route path='/cars' element={<Cars />} />
+      <Route path='/my-bookings' element={<MyBookings />} />
+    </Routes>
+    </>
   )
 }
 
---
+++ src/pages/MyBookings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyBookings = () => {
+    return (
+        <div>
+            MyBookings
+        </div>
+    )
+}
+
+export default MyBookings;
