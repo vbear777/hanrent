3ccc92328b7857bca9eb4ad673b69f5798209e99
Vbear <ryoitenkaii@gmail.com>
2026-04-27 12:15:06 +0700

    make Navbar component and edit logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     return (
         <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
             <Link to='/'>
-                <img src={assets.logo} alt="logo" className='h-8' />
+                <img src={assets.logo} alt="logo" className='h-15' />
             </Link>
 
             <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}>
@@ -18,6 +18,11 @@ const NavBar = () => {
                         {link.name}
                     </Link>
                 ))}
+
+                <div>
+                    <input type="text" className='py-1.5 w-full bg-transparent outline-none placeholder-gray-500' placeholder='Search Products' />
+                    <img src={assets.search_icon} alt="" />
+                </div>
             </div>
 
         </div>
