66d1dcb2d8d6f70d114748660d4a655889d29582
Vbear <ryoitenkaii@gmail.com>
2026-04-30 09:13:42 +0700

    make owner admin dashboard page and its section

--- src/components/owner/SideBar.jsx
+++ src/components/owner/SideBar.jsx
@@ -17,7 +17,7 @@ const SideBar = () => {
         <div className='relative min-h-screen md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-borderColor text-sm'>
             <div className='group relative'>
                 <label htmlFor="image">
-                    <img src={image ? URL.createObjectURL(image) : user?.image || "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=300"} alt="image"/>
+                    <img src={image ? URL.createObjectURL(image) : user?.image || "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=300"} alt="profile image" className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto'/>
                     <input type="file" id="image" accept='image/*' hidden onChange={e => setImage(e.target.files[0])} />
                     <div className='absolute hidden top-0 right-0 left-0 bottom-0 bg-black/10 rounded-full group-hover:flex items-center justify-center cursor-pointer'>
                         <img src={assets.edit_icon} alt="edit icon" />
