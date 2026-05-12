9416edc9a5156ba4d7e29367adf4b199c35fa37a
vbear <ryoitenkaii@gmail.com>
2026-05-12 16:25:58 +0700

    use dynamic real data instead of dummy data in owner component

--- src/components/owner/SideBar.jsx
+++ src/components/owner/SideBar.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
-import { assets, dummyUserData, ownerMenuLinks } from '../../assets/assets';
+import { assets, ownerMenuLinks } from '../../assets/assets';
 import { NavLink, useLocation } from 'react-router-dom';
+import { useAppContext } from '../../context/AppContext';
 
 const SideBar = () => {
 
-    const user = dummyUserData;
+    const { user, axios, fetchUser } = useAppContext();
     const location = useLocation()
     const [image, setImage] = useState('')
     
     const updateImage = async () => {
-        user.image = URL.createObjectURL(image)
-        setImage('')
+        try {
+            const formData = new FormData()
+            formData.append('image', image)
+
+            const {data} = await axios.post('/api/owner/update-image', formData)
+
+            if(data.success) {
+                fetchUser()
+                toast.success(data.message)
+                setImage('')
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
 
     return (
