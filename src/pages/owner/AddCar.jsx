57ef550228d6523944a412a3c50657d5e2c0ce41
vbear <ryoitenkaii@gmail.com>
2026-05-13 15:39:21 +0700

    add car feature with dynamic data and fix some error in addcar

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import Title from '../../components/owner/Title'
 import { assets } from '../../assets/assets'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const AddCar = () => {
-    const currency = import.meta.env.VITE_CURRENCY
+    const { axios, currency } = useAppContext();
 
     const [image, setImage] = useState(null)
     const [car, setCar] = useState({
@@ -19,8 +21,42 @@ const AddCar = () => {
         description: ''
     })
 
+    const [isLoading, setIsLoading] = useState(false)
     const onSubmitHandler = async (e) => {
         e.preventDefault();
+        if (isLoading) return null
+
+        setIsLoading(true)
+        try {
+            const formData = new FormData()
+            formData.append('image', image)
+            formData.append('carData', JSON.stringify(car))
+
+            const { data } = await axios.post('/api/owner/add-car', formData)
+
+            if (data.success){
+                toast.success(data.message)
+                setImage(null)
+                setCar({
+                    brand: '',
+                    model: '',
+                    year: 0,
+                    pricePerDay: 0,
+                    category: '',
+                    transmission: '',
+                    fuel_type: '',
+                    seating_capacity: 0,
+                    location: '',
+                    description: ''
+                })
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -112,7 +148,7 @@ const AddCar = () => {
 
                 <button className='flex items-center gap-2 px-4 py-2.5 mt-4 bg-dark-ocean text-white rounded-md font-medium w-max cursor-pointer'>
                     <img src={assets.tick_icon} alt="" />
-                    List Your Car
+                    {isLoading ? 'Listing...' : 'List Your Car'}
                 </button>
             </form>
         </div>
