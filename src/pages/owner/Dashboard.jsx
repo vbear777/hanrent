828d6fdcee441bc04de115dcf191ce5103a4537e
vbear <ryoitenkaii@gmail.com>
2026-05-13 14:50:42 +0700

    fetch dynamic real data on dashboard owner

--- src/pages/owner/Dashboard.jsx
+++ src/pages/owner/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { assets, dummyDashboardData } from '../../assets/assets'
 import Title from '../../components/owner/Title'
+import { useAppContext } from '../../context/AppContext'
 
 const Dashboard = () => {
 
-    const currency = import.meta.env.VITE_CURRENCY;
+    const {axios, isOwner, currency} = useAppContext();
 
     const [data, setData] = useState({
         totalCars: 0,
@@ -21,8 +22,24 @@ const Dashboard = () => {
         {title: "Pending", value: data.pendingBookings, icon: assets.cautionIconColored},
         {title: "Confirmed", value: data.completedBookings, icon: assets.listIconColored}
     ]
+
+    const fetchDashboardData = async () => {
+        try {
+            const {data} = await axios.get('/api/owner/dashboard')
+            if(data.success) {
+                setData(data.dashboardData)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
-        setData(dummyDashboardData)
+        if(isOwner){
+            fetchDashboardData()
+        }
     }, [])
 
     return (
--- src/pages/owner/Layout.jsx
+++ src/pages/owner/Layout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavbarOwner from '../../components/owner/NavBarOwner';
 import SideBar from '../../components/owner/SideBar';
 import { Outlet } from 'react-router-dom';
+import { useAppContext } from '../../context/AppContext';
 
 const Layout = () => {
+    const {isOwner, navigate} = useAppContext()
+
+    useEffect(() => {
+        if(!isOwner){
+            navigate('/')
+        }
+    }, [isOwner])
+
     return (
         <div className='flex flex-col'>
             <NavbarOwner />
