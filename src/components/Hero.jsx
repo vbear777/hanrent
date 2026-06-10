0839dbd23cc149164142f9e115866e0acf760f63
vbear <ryoitenkaii@gmail.com>
2026-06-10 09:27:36 +0700

    add motion animation on hero section and fix responsive display h1 floating away

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { assets, cityList } from '../assets/assets'
 import { useAppContext } from '../context/AppContext';
+import { motion } from 'motion/react';
 
 const Hero = () => {
     const [pickupLocation, setPickupLocation] = useState('');
@@ -13,10 +14,25 @@ const Hero = () => {
     }
 
     return (
-        <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
-            <h1 className='text-4xl md:text-5xl font-semibold'>Simple book on a rent cars platform</h1>
+        <motion.div 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ duration: 0.8 }}
+            className='min-h-screen flex flex-col items-center justify-center gap-8 md:gap-14 bg-light text-center px-4 py-8'>
+            <motion.h1 
+                initial={{y: 50, opacity: 0}}
+                animate={{y: 0, opacity: 1}}
+                transition={{ duration: 0.8, delay: 0.2 }}
+                className='text-4xl md:text-5xl font-semibold'>Simple book on a rent cars platform
+            </motion.h1>
 
-            <form onSubmit={handleSearch} className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
+            <motion.form
+                initial={{ scale: 0.95, opacity: 0, y: 50 }}
+                animate={{ scale: 1, opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: 0.4 }}
+                onSubmit={handleSearch} 
+                className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'
+            >
 
                 <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
                     <div className='flex flex-col items-start gap-2'>
@@ -36,14 +52,23 @@ const Hero = () => {
                     </div>
                 </div>
 
-                <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-gradient-to-r from-dark-ocean via-cream to-dark-ocean bg-[length:200%_100%] bg-left hover:bg-right transition-all duration-1000 text-white rounded-full cursor-pointer group'>
+                <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }} 
+                    className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-gradient-to-r from-dark-ocean via-cream to-dark-ocean bg-[length:200%_100%] bg-left hover:bg-right transition-all duration-1000 text-white rounded-full cursor-pointer group'>
                     <img src={assets.search_icon} alt="search" className='brightness-300'/>
                     <span>Search</span>
-                </button>
-            </form>
+                </motion.button>
+            </motion.form>
 
-            <img src={assets.main_car} alt="car" className='max-h-74' />
-        </div>
+            <motion.img 
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.6 }}
+                src={assets.main_car} 
+                alt="car" 
+                className='max-h-74' />
+        </motion.div>
     )
 }
 
