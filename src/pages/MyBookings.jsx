ddfa9db2d46d67626b076b12eedc92d9c44918c7
vbear <ryoitenkaii@gmail.com>
2026-06-10 08:37:16 +0700

    change mybookings page using dynamic data

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { assets, dummyCarData, dummyMyBookingsData } from '../assets/assets';
 import Title from '../components/Title';
+import { useAppContext } from '../context/AppContext';
 
 const MyBookings = () => {
+
+    const { axios, user, currency } = useAppContext()
+
     const [bookings, setBookings] = useState([]);
-    const currency = import.meta.env.VITE_CURRENCY
 
     const fetchMyBookings = async () => {
-        setBookings(dummyMyBookingsData)
+        try {
+            const { data } = await axios.get('/api/bookings/user')
+            if (data.success){
+                setBookings(data.bookings)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
 
     useEffect(() => {
-        fetchMyBookings()
-    }, [])
+        user && fetchMyBookings()
+    }, [user])
 
     return (
         <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl mb-10'>
